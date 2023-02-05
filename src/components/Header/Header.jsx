66176a13fc3063b03b081f057c6e21b8eaae9c2e
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-02-05 21:08:38 +0100

    [Fix]: padding bug

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import Logo from "../Logo/Logo";
 const Header = () => {
   return (
     <div className="mb-[0px] flex items-center bg-gray-dark-27">
-      <div className="mr-auto p-[24px] isolate">
+      <div className="mr-auto p-[23px] isolate">
         <img
           className="absolute top-0 left-0 z-[-1]"
           src={images.logoBg}
@@ -13,11 +13,11 @@ const Header = () => {
         />
         <Logo />
       </div>
-      <div className="p-[24px] p-b-[10px]">
+      <div className="p-[20px]">
         <Icon className="" id="moon-icon" color="" size="" strokeWidth={""} />
       </div>
-      <div className="border-l-[0.01px] h-[72px] border-[#494E6E]"></div>
-      <div className="p-[24px]">
+      <div className="border-l-[0.01px] h-[64px] border-[#494E6E]"></div>
+      <div className="p-[18px]">
         <img
           className="rounded-full w-[32px]"
           src={images.AvatarImage}
