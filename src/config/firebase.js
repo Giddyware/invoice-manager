e94a443f992286b183a771ace0b7e597f6bffa4e
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-03-05 13:32:38 +0100

    fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,7 +5,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  apiKey: 'AIzaSyDQcaGg-6jPnRkOPNVclzKWEdvKIwv7KTM',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
