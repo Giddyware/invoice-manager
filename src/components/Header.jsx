1c9ac9ee0fc9c7282e807b7cc18a897759f29a33
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-04-04 00:41:11 +0100

    Feature: setup the darkMode toggler

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { images } from "../constants";
 import { Authentication } from "./auth";
 // import { Auth } from "./auth";
-import Icon from "./Icon";
+// import Icon from "./Icon";
 import Logo from "./Logo";
 import ThemeModeToggler from "./ThemeModeToggler";
 
@@ -10,13 +10,26 @@ const Header = () => {
   const [showDialog, setShowDialog] = useState(false);
   const close = () => setShowDialog(false);
 
+  // const [theme, setTheme] = useState("light");
+
+  // useEffect(() => {
+  //   if (theme === "dark") {
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // }, [theme]);
+
+  // const handleThemeSwitch = ()=>{
+  //   setTheme(theme === 'dark'? 'light':'dark')
+  // }
+
   return (
     <div className="mb-[0px] flex items-center bg-brand-500 lg:flex-col lg:min-h-screen lg:rounded-tr-[22px]">
       <Logo />
 
       <div className="p-[20px] lg:mt-auto">
         <ThemeModeToggler />
-        
       </div>
       <div className="border-l-[0.01px] h-[64px] border-[#494E6E] lg:hidden"></div>
       <hr className="w-[64px] text-[#494E6E] md:hidden sm:hidden"></hr>
--- src/components/ThememodeToggler.jsx
+++ src/components/ThememodeToggler.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from "react";
 import Icon from "./Icon";
 
 const ThemeModeToggler = () => {
+  const [theme, setTheme] = useState(null);
+
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+  }, []);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
   return (
-    <>
-      <Icon className="" id="moon-icon" color="" size="" strokeWidth={""} />
-      <Icon className="" id="sun-icon" color="" size="" strokeWidth={""} />
-    </>
+    <div onClick={handleThemeSwitch}>
+      {theme === "dark" ? (
+        <Icon
+          className=""
+          // onClick={handleThemeSwitch}
+          id="moon-icon"
+          color=""
+          size=""
+          strokeWidth={""}
+        />
+      ) : (
+        <Icon className="" id="sun-icon" color="" size="" strokeWidth={""} />
+      )}
+    </div>
   );
 };
 export default ThemeModeToggler;
