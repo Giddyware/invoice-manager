2251a080ec0c2cced3b2298be3d52b042423f534
Giddy <Idowugidiyanuoluwa@gmail.com>
2023-04-04 12:07:21 +0100

    feature: ThemeModeToggler added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import { Authentication } from "./auth";
 // import Icon from "./Icon";
 
 import Logo from "./Logo";
+import ThemeModeToggler from "./ThemeModeToggler";
 
 const Header = () => {
   const [showDialog, setShowDialog] = useState(false);
@@ -28,7 +29,9 @@ const Header = () => {
     <div className="mb-[0px] flex items-center bg-brand-500 lg:flex-col lg:min-h-screen lg:rounded-tr-[22px]">
       <Logo />
 
-      <div className="p-[20px] lg:mt-auto">{/* <ThemeModeToggler /> */}</div>
+      <div className="p-[20px] lg:mt-auto">
+        <ThemeModeToggler />
+      </div>
       <div className="border-l-[0.01px] h-[64px] border-[#494E6E] lg:hidden"></div>
       <hr className="w-[64px] text-[#494E6E] md:hidden sm:hidden"></hr>
       <div onClick={() => setShowDialog(true)} className="p-[18px]">
